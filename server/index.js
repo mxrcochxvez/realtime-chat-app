5c121923c2fdb450e5217b9e477c9d8acae295cb
Marco Chavez <marcode.chavez.jr@gmail.com>
2020-06-22 11:17:44 -0700

    adding project files

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const PORT = process.env.PORT || 5000;
+
+const router = require('./router');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log("We have a new connection!");
+
+    socket.on('join', ({ name, room }, callback) => {
+        console.log(name, room);
+
+        // if(error) {
+        //     callback({ error });
+        // }
+    })
+    
+
+    socket.on('disconnect', () => {
+        console.log("We have lost a connection.")
+    })
+});
+
+app.use(router);
+
+server.listen(PORT, () => console.log(`Server is listening on *:${PORT}`));
